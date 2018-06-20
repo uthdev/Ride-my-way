6de216aa3fe545eccd26609e4dd4e80693b072d5
joeeasy <jehonadabokpus@gmail.com>
2018-06-20 23:21:09 +0100

    Feature user can send ride request
    [Finishes #158472782] User should be able to join ride

---
+++ server/src/dummydata/riderequest.js
@@ -0,0 +1,35 @@
+const rideRequestDb = [
+
+  {
+    rideId: 1,
+    noOfSeats: 2,
+    passengersId: [2, 4],
+    noOfSeatsLeft: 0,
+  },
+  {
+    rideId: 2,
+    noOfSeats: 4,
+    passengersId: [2, 4],
+    noOfSeatsLeft: 2,
+  },
+  {
+    rideId: 3,
+    noOfSeats: 4,
+    passengersId: [4],
+    noOfSeatsLeft: 3,
+  },
+  {
+    rideId: 4,
+    noOfSeats: 4,
+    passengersId: [],
+    noOfSeatsLeft: 4,
+  },
+  {
+    rideId: 5,
+    noOfSeats: 4,
+    passengersId: [2, 3],
+    noOfSeatsLeft: 2,
+  },
+];
+
+export default rideRequestDb;
