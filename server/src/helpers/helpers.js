3ac39edaaccf9844e302c7a4834696b1ff172581
joeeasy <jehonadabokpus@gmail.com>
2018-06-23 22:38:07 +0100

    Chore clean and optimize rideOffer controller
    [Finishes #158577230] Clean up the ride offer controller to make it more readable

---
+++ server/src/helpers/helpers.js
@@ -0,0 +1,33 @@
+
+const helpers = {};
+
+/* Check the id of every request to see whether it can be converted to number */
+helpers.parsedId = id => ((!(/^\d+$/.test(id))) ? NaN : parseInt(id, 10));
+
+/* Validate fields for create new ride offers */
+helpers.isValid = (rideOffer) => {
+  // validations
+  const name = rideOffer.name.trim() !== '';
+  const location = rideOffer.location.trim() !== '';
+  const destination = rideOffer.destination.trim() !== '';
+  const departureTime = rideOffer.departureTime.trim() !== '';
+  const price = typeof (rideOffer.price) === 'number' && rideOffer.price > 0;
+
+  return name && location && destination && departureTime && price;
+};
+
+/* returns the response of a request */
+helpers.error = (res, statusCode, message) => res.status(statusCode).json({ message });
+
+/* When a request was successful */
+helpers.success = (res, statusCode, message, data) =>
+  res.status(statusCode)
+    .json({ message, data });
+
+/* Find ride offer */
+helpers.find = (rideOfferArr, id) => rideOfferArr.find(ride => (id === ride.id));
+
+/* Find request */
+helpers.findRequest = (requestArr, id) => requestArr.find(request => (id === request.rideId));
+export default helpers;
+
