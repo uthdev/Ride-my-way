8bbe35ca2d1bab155781428eb8727a08e1fbdf98
joeeasy <jehonadabokpus@gmail.com>
2018-06-26 22:26:04 +0100

    Refactor helpers functions
    [Finishes #158645901] Refactor helpers functions to follow the single responsibility principle

--- server/src/controllers/RideControllers.js
+++ server/src/controllers/RideControllers.js
@@ -1,6 +1,6 @@
 import rideOffersDb from '../dummydata/rideoffers';
 import rideRequestDb from '../dummydata/riderequest';
-import helpers from '../helpers/helpers';
+import { success, error, parsedInt, isValid, isRequestValid, findRequest, find, isSeatValid } from '../helpers/rideHelpers';
 
 /**
  * Processes all ride data
@@ -13,7 +13,7 @@ class RideController {
    * @returns {Object} Array ride offers
    */
   static allRideOffer(req, res) {
-    return (rideOffersDb.length > 0) && helpers.success(res, 200, 'All ride Offers', rideOffersDb);
+    return (rideOffersDb.length > 0) && success(res, 200, 'All ride Offers', rideOffersDb);
   }
 
   /**
@@ -24,18 +24,18 @@ class RideController {
   static getRideOffer(req, res) {
     const { id } = req.params;
 
-    const parsedId = helpers.parsedId(id);
+    const parsedId = parsedInt(id);
     let rideDetails = '';
 
     /* Check if id is  a Not a number */
     if (!(Number.isInteger(parsedId))) {
-      return helpers.error(res, 400, 'Ride id is invalid');
+      return error(res, 400, 'Ride id is invalid');
     }
 
-    rideDetails = helpers.find(rideOffersDb, parsedId);
+    rideDetails = find(rideOffersDb, parsedId);
     // if ride is found rerurn ride
-    if (rideDetails) return helpers.success(res, 200, 'Found a ride', rideDetails);
-    return helpers.error(res, 404, 'The ride offer you requested does not exist');
+    if (rideDetails) return success(res, 200, 'Found a ride', rideDetails);
+    return error(res, 404, 'The ride offer you requested does not exist');
   }
 
   /**
@@ -47,14 +47,14 @@ class RideController {
     const rideOffer = req.body;
 
     // check is ride is valid
-    if (helpers.isValid(rideOffer)) {
+    if (isValid(rideOffer)) {
       rideOffersDb.push({
         id: rideOffersDb.length + 1,
         ...rideOffer,
       });
-      return helpers.success(res, 201, 'New ride offer has been created', rideOffersDb);
+      return success(res, 201, 'New ride offer has been created', rideOffersDb);
     }
-    return helpers.error(res, 400, 'Please enter the missing fields');
+    return error(res, 400, 'Please enter the missing fields');
   }
 
   /**
@@ -64,7 +64,7 @@ class RideController {
   static joinRide(req, res) {
     // Ride id
     const { id } = req.params;
-    const parsedId = helpers.parsedId(id);
+    const parsedId = parsedInt(id);
     // reques body
     const requestOption = req.body;
     // ride request to be sent to ride owner
@@ -80,15 +80,14 @@ class RideController {
 
 
     // Check if there is space for additional ride offers
-    if (helpers.isRequestValid(requestOption)) {
-      rideRequest = helpers.findRequest(rideRequestDb, parsedId);
+    if (isRequestValid(requestOption)) {
+      rideRequest = findRequest(rideRequestDb, parsedId);
       // re-assign use variables
       noOfSeats = rideRequest.noOfSeats + 1;
       passengerArr = rideRequest.passengersId.length;
 
       // check to see if passengers seats are already occuppied
-      if ((noOfSeats > passengerArr)
-       && (noOfSeats > (passengerArr + userSeat))) {
+      if (isSeatValid(noOfSeats, passengerArr, userSeat)) {
         // Add passenger multiple passengers for one user
         while (userSeat > index) {
           rideRequest.passengersId.push(requestOption.id);
@@ -96,11 +95,11 @@ class RideController {
         }
 
         rideRequest.noOfSeatsLeft = (rideRequest.noOfSeats) - (rideRequest.passengersId.length);
-        return helpers.success(res, 200, 'Ride request sent', rideRequest);
+        return success(res, 200, 'Ride request sent', rideRequest);
       }
-      return helpers.error(res, 201, 'Your cannot join this ride the passengers are already complete');
+      return error(res, 201, 'Your cannot join this ride the passengers are already complete');
     }
-    return helpers.error(res, 400, 'Invalid request token');
+    return error(res, 400, 'Invalid request token');
   }
 }
 
