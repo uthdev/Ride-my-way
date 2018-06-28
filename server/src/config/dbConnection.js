c716c735b45b46734c39168ece4fd7372bec0f13
joeeasy <jehonadabokpus@gmail.com>
2018-06-28 21:13:07 +0100

    Revert "Setup database"

--- server/src/config/dbConnection.js
+++
@@ -1,6 +0,0 @@
-import { Pool } from 'pg';
-import dbConfig from './databaseConfig';
-
-const dbPool = (process.NODE_ENV === 'production') ? new Pool(dbConfig.database) : new Pool(dbConfig.test);
-
-export default dbPool;
