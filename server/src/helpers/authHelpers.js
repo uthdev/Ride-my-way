6faf5cf7aec02c438436b01afb6d4a9a5315738d
joeeasy <jehonadabokpus@gmail.com>
2018-06-30 07:53:41 +0100

    Feedback change variable naming convention
    [Finishes #158725007] Your variable naming convention should be more human readable

--- server/src/helpers/authHelpers.js
+++ server/src/helpers/authHelpers.js
@@ -1,16 +1,17 @@
-import dbPool from '../config/dbConnection';
 
 /**
  * @description Checks if User data a correct
  * @param {Object} userDetails
  * @param {Object} res
+ * @returns {Boolean} true or false expecting true
  */
 export const isUserDetailsValid = (userDetails, res) => {
-  const fname = (userDetails.fname.trim() !== '') ? userDetails.fname :
+  const firstName = (userDetails.firstName.trim() !== '') ? userDetails.firstName :
     res.status(400).json({
-      error: 'First name cannot be empty',
+      status: 'fail',
+      message: 'First name cannot be empty',
     });
-  const lname = (userDetails.lname.trim() !== '') ? userDetails.lname :
+  const lastName = (userDetails.lastName.trim() !== '') ? userDetails.lastName :
     res.status(400).json({
       error: 'Last name cannot be empty',
     });
@@ -18,15 +19,15 @@ export const isUserDetailsValid = (userDetails, res) => {
     res.status(400).json({
       error: 'Email is required',
     });
-  const phone = (userDetails.phone.trim() !== '' && userDetails.phone.length === 11) ? userDetails.phone :
+  const phone = (userDetails.phone.trim() !== '') ? userDetails.phone :
     res.status(400).json({
       error: 'Phone number invalid(Phone number cannot be less  than 11 digits or greater',
     });
-  const password = (userDetails.password.trim() !== '' && userDetails.password >= 6) ? userDetails.password :
+  const password = (userDetails.password.trim() !== '' && userDetails.password.length >= 6) ? userDetails.password :
     res.status(400).json({
       error: 'Password cannot be empty or Less than 6 characters',
     });
-  const cPassword = (userDetails.password.trim() === userDetails.cPassword.trim()) ?
+  const confirmPassword = (userDetails.password.trim() === userDetails.confirmPassword.trim()) ?
     userDetails.password :
     res.status(400).json({
       error: 'Password does not match',
@@ -38,19 +39,18 @@ export const isUserDetailsValid = (userDetails, res) => {
     error: 'Zip code is required',
   });
 
-  return (fname && lname && email && phone && password && cPassword && address && zip);
+  return (firstName && lastName && email && phone && password && confirmPassword && address && zip);
 };
 
 /**
- * @descriptionChecks if User is alrady existing
- * @param {string} userEmail
+ * @description Checks if User is alrady existing
+ * @param {string} data
  */
-export const isUserExisting = (userEmail, res) => {
-  let isExisting = false;
-  dbPool.query(`SELECT email FROM users WHERE email = '${userEmail}'`)
-    .then((response) => {
-      console.log(response.rows.email.trim());
-      isExisting = response.rowCount > 0;
-    }).catch(e => console.log(e.stack));
-  return isExisting;
+export const isUserValid = (data, res) => {
+  const email = (data.email.trim() !== '') ? data.email : res.status(400).json({
+    error: 'Email not valid',
+  });
+  const password = (data.password.trim() !== '') ? data.password : res.status(400).json({ error: 'Password is required' });
+
+  return email && password;
 };
