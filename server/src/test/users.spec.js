263322543128f25ff6e99df83c3405ecda00ccd8
joeeasy <jehonadabokpus@gmail.com>
2018-07-05 12:29:21 +0100

    test all auth routes
    - ensure that a user can create an account
    - ensure that an already register user cannot re-create account
    - implement error handling when some goes wrong with the server
    - ensure that error are handled when user credentials are not valid
    - ensure that user can login to their account successfully
    - ensure that exception are handle when there's a database connection issue
    
    [Finishes #158837018]

---
+++ server/src/test/users.spec.js
@@ -0,0 +1,137 @@
+import chai, { assert, expect } from 'chai';
+import chaiHtpp from 'chai-http';
+
+import app from '../index';
+import { user1 } from './tokens';
+
+/* Test all ride actions */
+
+
+chai.use(chaiHtpp);
+describe('Test users signup routes', () => {
+  /* Allows a user to create an account */
+  describe('/GET api/v1/auth/signup', () => {
+    it('users should be able to create an account', (done) => {
+      const data = {
+        firstName: 'trust',
+        lastName: 'jamine',
+        phone: '07059972180',
+        email: 'jokpus@gmail.com',
+        password: 'joeeasy',
+        confirmPassword: 'joeeasy',
+        address: '42 montgomery',
+        city: 'Yaba',
+        zipCode: '101212',
+      };
+      chai.request(app)
+        .post('/api/v1/auth/signup')
+        .send(data)
+        .end((error, res) => {
+          expect(res).to.have.status(201);
+          expect(res.body.data.message).to.equal('Your account was created successfully');
+          done();
+        });
+    });
+  });
+  /* user can't sign up when he has already created an account */
+  describe('/GET api/v1/auth/signup', () => {
+    it('users should not be able to create an account twice', (done) => {
+      const data = {
+        firstName: 'trust',
+        lastName: 'jamine',
+        phone: '07059972180',
+        email: 'jokpus@gmail.com',
+        password: 'joeeasy',
+        confirmPassword: 'joeeasy',
+        address: '42 montgomery',
+        city: 'Yaba',
+        zipCode: '101212',
+      };
+      chai.request(app)
+        .post('/api/v1/auth/signup')
+        .send(data)
+        .end((error, res) => {
+          expect(res).to.have.status(403);
+          expect(res.body.data).to.equal('A user with this email address already exist');
+          done();
+        });
+    });
+  });
+  /* user shouldn't be able to create an account if his details is not valid */
+  describe('/GET api/v1/auth/signup', () => {
+    it('users cannot create an account when credentials are not complete', (done) => {
+      const data = {
+        firstName: '',
+        lastName: 'jamine',
+        phone: '07059972180',
+        email: 'jokpus@gmail.com',
+        password: 'joeeasy',
+        confirmPassword: 'joeeasy',
+        address: '42 montgomery',
+        city: 'Yaba',
+        zipCode: '101212',
+      };
+      chai.request(app)
+        .post('/api/v1/auth/signup')
+        .send(data)
+        .end((error, res) => {
+          expect(res).to.have.status(401);
+          done();
+        });
+    });
+  });
+});
+
+/* User signin */
+describe('Test users signin routes', () => {
+  /* user can't sign up when he has already created an account */
+  describe('/GET api/v1/auth/signin', () => {
+    it('users should not be able to login in when wrong credentials are provided', (done) => {
+      const data = {
+        email: 'jokpus@gmail.com',
+        password: 'joeeas0000y',
+      };
+      chai.request(app)
+        .post('/api/v1/auth/signin')
+        .send(data)
+        .end((error, res) => {
+          expect(res).to.have.status(401);
+          // expect(res.body.data).to.equal('Username or password is not correct');
+          done();
+        });
+    });
+  });
+  /* user shouldn't be able to sign when credentials are empty */
+  describe('/GET api/v1/auth/signin', () => {
+    it('users should not be able to signin when credentials are incomplete', (done) => {
+      const data = {
+        email: 'jokpus@gmail.com',
+        password: '',
+      };
+      chai.request(app)
+        .post('/api/v1/auth/signin')
+        .send(data)
+        .end((error, res) => {
+          expect(res).to.have.status(401);
+          done();
+        });
+    });
+  });
+  /* Allows a user to create an account */
+  describe('/GET api/v1/auth/signin', () => {
+    it('users should be able to login account to their account', (done) => {
+      const data = {
+        email: 'jokpus@gmail.com',
+        password: 'joeeasy',
+      };
+      chai.request(app)
+        .post('/api/v1/auth/signin')
+        .send(data)
+        .end((error, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body.data.message).to.equal('User login successfull');
+          done();
+        });
+    });
+  });
+});
