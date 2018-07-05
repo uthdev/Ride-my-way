0a441d9113e95d65336d69adf9196b39ebef7d24
joeeasy <jehonadabokpus@gmail.com>
2018-07-05 14:22:42 +0100

    bug(travis.yml) parsing error on travis configuaration

--- server/src/config/dbConnection.js
+++ server/src/config/dbConnection.js
@@ -2,7 +2,5 @@ import { Pool } from 'pg';
 import dbConfig from './databaseConfig';
 
 const dbPool = (process.env.NODE_ENV === 'test') ? new Pool(dbConfig.test) : new Pool(dbConfig.database);
-console.log(dbPool);
-console.log(process.env);
 
 export default dbPool;
