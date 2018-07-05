fe02b562568a178278eb78cd63d72f619d95284e
joeeasy <jehonadabokpus@gmail.com>
2018-07-05 17:39:18 +0100

    bug(travis) still failing

--- server/src/config/dbConnection.js
+++ server/src/config/dbConnection.js
@@ -2,5 +2,7 @@ import { Pool } from 'pg';
 import dbConfig from './databaseConfig';
 
 const dbPool = (process.env.NODE_ENV === 'test') ? new Pool(dbConfig.test) : new Pool(dbConfig.database);
+console.log(dbPool);
+
 
 export default dbPool;
