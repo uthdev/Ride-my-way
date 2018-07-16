76094d658c4703e560e04b82fb170a0aa521733f
joeeasy <jehonadabokpus@gmail.com>
2018-07-16 12:55:45 +0100

    refactor auth for cleint-side
    - remove redudant function
    
    -format sign and sign up for easy readability
    
    -refactor front-end to mean requirements

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,11 +1,29 @@
 const app = {};
 
+/**
+ * @method saveToken Saves the token on local storage
+ * @param {string} token
+ */
+app.saveToken = token => localStorage.setItem('token', token);
+
+/**
+ * @method getToken
+ * @description fetches token from local storage
+ * @returns token
+ */
+app.getToken = () => localStorage.getItem('token');
+
+/**
+ * @method loadSideBar
+ * @description loads hamburger menu once the window loads
+ */
 app.loadSideBar = () => {
   const btns = Array.from(document.getElementsByClassName('js__navbar__toggler'));
   const navBarCollaspe = document.querySelector('.navbar__collapse');
 
   btns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
+      e.preventDefault();
       if (navBarCollaspe.style.display === 'none') {
         navBarCollaspe.style.display = 'block';
       } else {
@@ -15,13 +33,36 @@ app.loadSideBar = () => {
   });
 };
 
+/* Get data from the form body */
+/**
+ * @method getFormData
+ * @description fetches the data from the form and creates the request body
+ * @returns request body || form body
+ */
+app.getFormData = () => {
+  const form = document.getElementsByTagName('form')[0];
+  const inputs = form.getElementsByTagName('input');
+
+  const formData = {};
+  for (let i = 0; i < inputs.length; (i += 1)) {
+    formData[inputs[i].name] = inputs[i].value;
+  }
+  return formData;
+};
+
 /* sign in and signup user helpers */
-app.authuUser = (url, formData) =>
-// Default options are marked with *
+/**
+ * @method authUser
+ * @param {string} url
+ * @param {string} formData
+ * @description allows users to login or sign up
+ */
+app.authUser = (url, formData) =>
+  // Default options are marked with *
   fetch(url, {
     method: 'POST', // *GET, POST, PUT, DELETE, etc
     headers: {
-    // Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NywiaWF0IjoxNTMxMzE0OTc5LCJleHAiOjE1MzEzOTg5Nzl9.ygYphH_SlLiPJ6O_TCT2LJ85MjRemBvd7GZw02M3Cbc',
+
       'Content-Type': 'application/json',
     },
     redirect: 'follow', // manual, *follow, error
@@ -29,23 +70,24 @@ app.authuUser = (url, formData) =>
     body: formData,
   })
     .then(response => response.json()) // parses response to JSON
+    // eslint-disable-next-line
     .catch(error => console.error('Fetch Error =\n', error));
 
+/* Createa an account for a new user */
+/**
+ * @method signUp
+ * @description Allows users to create an account
+ * @returns user information and auth token and save's it to localstorage
+ */
 app.signUp = () => {
   const form = document.getElementsByTagName('form')[0];
   const errMsg = document.querySelector('.js__errMsg');
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const inputs = form.getElementsByTagName('input');
-
-    const formData = {};
-    for (let i = 0; i < inputs.length; i++) {
-      formData[inputs[i].name] = inputs[i].value;
-    }
+    const formData = app.getFormData();
     const formdata = JSON.stringify(formData);
-    app.authuUser('/api/v1/auth/signup', formdata).then((data) => {
-      console.log(data);
+    app.authUser('/api/v1/auth/signup', formdata).then((data) => {
       if (data.status === 'fail') {
         errMsg.textContent = `* ${data.data}`;
       }
@@ -55,29 +97,34 @@ app.signUp = () => {
       if (data.status === 'success') {
         errMsg.style.color = '#78c078';
         errMsg.textContent = `* ${data.data.message}, You will be redirected shortly`;
-        setTimeout(() => (window.location = 'summary.html'), 1500);
+
+        app.saveToken(data.data.token);
+        setTimeout(() => {
+          window.location = 'summary.html';
+          return null;
+        }, 1500);
       }
-    }) // JSON from `response.json()` call
+    }) // eslint-disable-next-line
       .catch(error => console.error(error));
   });
 };
 
 /* user sign in  */
+/**
+ * @method signin
+ * @description  Allows users to login to the application
+ * @returns user's info and saves the auth token to localstorage
+ */
 app.signIn = () => {
   const form = document.getElementsByTagName('form')[0];
   const errMsg = document.querySelector('.js__errMsg');
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const inputs = form.getElementsByTagName('input');
 
-    const formData = {};
-    for (let i = 0; i < inputs.length; i++) {
-      formData[inputs[i].name] = inputs[i].value;
-    }
+    const formData = app.getFormData();
     const formdata = JSON.stringify(formData);
-    app.authuUser('/api/v1/auth/signin', formdata).then((data) => {
-      console.log(data);
+    app.authUser('/api/v1/auth/signin', formdata).then((data) => {
       if (data.status === 'fail') {
         errMsg.textContent = `* ${data.data}`;
       }
@@ -87,12 +134,20 @@ app.signIn = () => {
       if (data.status === 'success') {
         errMsg.style.color = '#78c078';
         errMsg.textContent = `* ${data.data.message}, You will be redirected shortly`;
-        setTimeout(() => (window.location = 'summary.html'), 1500);
+        app.saveToken(data.data.token);
+        setTimeout(() => {
+          window.location = 'summary.html';
+          return null;
+        }, 1500);
       }
     }) // JSON from `response.json()` call
+    // eslint-disable-next-line
       .catch(error => console.error(error));
   });
 };
+
+
+/* Fetch all ride offers */
 
 
 // Get the navbar
@@ -100,8 +155,7 @@ const navbar = document.querySelector('nav');
 
 
 // Get the offset position of the navbar
-const sticky = 5;
-console.log(sticky);
+const sticky = 1;
 
 // When the user scrolls the page, execute myFunction
 window.onscroll = () => ((window.pageYOffset >= sticky) ? navbar.classList.add('sticky') : navbar.classList.remove('sticky'));
